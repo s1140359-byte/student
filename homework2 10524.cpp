de6e23af121761110e2b6f368d34685ba9192fa7
s1140359-byte <s1140359@dmhs.kh.edu.tw>
2025-10-27 16:18:05 +0800

    Add files via upload

---
+++ homework2 10524.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 15;
+    double height = 150;
+    std::string name = "張庭軒";
+    bool is_tudent = false;
+    std::cout << "張庭軒: " << name << std::endl;
+    std::cout << "張庭軒: " << name << std::endl;
+    std::cout << "張庭軒: " << name << std::endl;
+    std::count << "是否為學生: " <<  is_student << std::endl;
+    return 0;
+}
